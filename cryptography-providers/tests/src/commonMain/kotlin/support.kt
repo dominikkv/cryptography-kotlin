12ccca7668eb49fbc7a795c2be3b046ee7e8eff0
Oleg Yukhnevich <whyoleg@gmail.com>
2026-02-18 19:34:00 +0200

    [webcrypto] Make tests running in Safari in addition to Chrome and handle this in tests

--- cryptography-providers/tests/src/commonMain/kotlin/support.kt
+++ cryptography-providers/tests/src/commonMain/kotlin/support.kt
@@ -56,7 +56,8 @@ fun AlgorithmTestScope<*>.supportsFormat(format: EncodingFormat): Boolean = supp
         // only WebCrypto supports JWK for now
         format.name == "JWK" && !provider.isWebCrypto
              -> "JWK key format"
-        format == EC.PublicKey.Format.RAW.Compressed && provider.isApple
+        format == EC.PublicKey.Format.RAW.Compressed &&
+                (provider.isApple || provider.isWebCrypto && platform.isBrowserSafari)
              -> "compressed key format"
         else -> null
     }
@@ -87,10 +88,25 @@ fun AlgorithmTestScope<out AES<*>>.supportsKeySize(keySizeBits: Int): Boolean = 
     }
 }
 
-fun AlgorithmTestScope<RSA.PSS>.supportsSaltSize(saltSize: Int?): Boolean = supports {
+// we do test only safari and chrome
+fun AlgorithmTestScope<*>.supportsDataInput(inputSize: Int): Boolean = supports {
     when {
-        provider.isApple && saltSize != null -> "custom saltSize"
-        else                                 -> null
+        inputSize == 0 &&
+                provider.isWebCrypto && platform.isBrowserSafari -> "empty data input"
+        else                                                     -> null
+    }
+}
+
+fun AlgorithmTestScope<RSA.PSS>.supportsSaltSize(saltSize: Int?, digestSize: Int): Boolean = supports {
+    when {
+        saltSize != null &&
+                provider.isApple                                 -> "custom saltSize"
+        saltSize != null && saltSize > digestSize &&
+                provider.isWebCrypto && platform.isBrowserSafari -> {
+            println("SKIPPED[supportsSaltSize]: $this\n")
+            "to big saltSize"
+        }
+        else                                                     -> null
     }
 }
 
@@ -182,7 +198,7 @@ fun AlgorithmTestScope<out EC<*, *, *>>.supportsPrivateKeyDecoding(
     when {
         provider.isApple
                 && format == EC.PrivateKey.Format.RAW -> "private key '$format' format"
-        provider.isApple
+        (provider.isApple || provider.isWebCrypto && platform.isBrowserSafari)
                 && !hasPublicKey()                    -> "private key '$format' format without 'publicKey' from ${otherContext.provider}"
         else                                          -> null
     }
